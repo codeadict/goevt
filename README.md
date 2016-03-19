e99b3e3094d69e795e714958b1e666c1f32f1e6f
Dairon Medina <dairon.medina@gmail.com>
2016-03-19 00:23:34 -0400

    Adding initial project structure

---
+++ README.md
@@ -0,0 +1,18 @@
+[![Build Status](https://travis-ci.org/codeadict/goevt.svg?branch=master)](https://travis-ci.org/codeadict/goevt)
+
+# GoEVT
+
+GoEVT is a Go client library for accessing the Evrythng REST API.
+
+Documentation is available on : [godoc](http://godoc.org/github.com/codeadict/goevt)
+
+You can view Everythng API docs here: [https://dashboard.evrythng.com/documentation/api](https://dashboard.evrythng.com/documentation/api)
+
+## Usage
+
+##Authors:
+
+  * Dairon Medina C. (dairon.medina@gmail.com)
+
+##Found a bug?
+  Awesome, let me know! Send a pull request or a patch. Ask! I'm here to help and will respond to all filed issues.
---
+++ goevt.go
@@ -0,0 +1,40 @@
+//
+// Copyright 2016 Dairon Medina <http://github.com/codeadict>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package goevt
+
+import (
+  "bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+  libraryVersion = "0.0.1"
+	defaultBaseURL = "https://api.evrythng.com"
+	userAgent      = "goevt/" + libraryVersion
+	responseType   = "application/json"
+)
+
+// Client manages all the communication with Everythng API.
+type Client struct {
+  // The HTTP client that communicates with the API.
+  client *http.Client
+
+    
+}
