95248a40118fc415eb1f4968bb35646cafaf5579
Dairon Medina <dairon.medina@gmail.com>
2016-04-20 17:41:11 -0400

    Adding some tests

---
+++ goevt_test.go
@@ -0,0 +1,27 @@
+// Copyright 2016 Dairon Medina <http://github.com/codeadict>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package goevt
+
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestResourceUrl(t *testing.T) {
+	c := NewClient("http://base_url/", "api_path", "token")
+
+	assert.Equal(t, c.ResourceUrl(projects_url, nil), "http://base_url/api_path/projects?private_token=token")
+	assert.Equal(t, c.ResourceUrl(project_url, map[string]string{":id": "123"}), "http://base_url/api_path/projects/123?private_token=token")
+}
